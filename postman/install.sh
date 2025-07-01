36015c5eaafc7ae274b25b277c0a1d3b38de726e
Willian Yassunaka <wyassunaka@uolinc.com>
2025-07-01 17:41:28 -0300

    postman already installed

--- postman/install.sh
+++ postman/install.sh
@@ -2,13 +2,12 @@
 set -e
 source "$(dirname "$0")/../utils.sh"
 
-POSTMAN_DIR="/opt/Postman"
-if [ ! -d "$POSTMAN_DIR" ]; then
+if command -v postman >/dev/null 2>&1; then
+    echo "✅ Postman já instalado."
+else
     echo "📥 Instalando Postman..."
     wget -q https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
     sudo tar -xzf postman.tar.gz -C /opt
     sudo ln -sf /opt/Postman/Postman /usr/bin/postman
     rm postman.tar.gz
-else
-    echo "✅ Postman já instalado."
 fi
