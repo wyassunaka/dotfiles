4777d1112fcc5b62bbb1198f67267f31e8376277
Willian Yassunaka <willianhy@gmail.com>
2025-07-03 22:10:22 -0300

    fix utils

--- utils.sh
+++ utils.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function is_installed() {
-    command -v "$1" &> /dev/null
+    command -v "$1" >/dev/null 2>&1
 }
 
 function install_package() {
     local pkg="$1"
-    if dpkg -s "$pkg" &>/dev/null; then
+    if dpkg -s "$pkg" >/dev/null 2>&1; then
         echo "📦 $pkg já está instalado."
     else
         echo "📥 Instalando $pkg..."
